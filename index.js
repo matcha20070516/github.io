9a5e4a1d1981b417197af5223433deb7b0f747d7
matcha20070516 <ma0516_hogreen@icloud.com>
2025-05-07 15:08:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-  <script>
-    function start() {
-      const name = document.getElementById("name").value.trim();
-      const set = document.getElementById("set").value;
+function start() {
+  const name = document.getElementById("name").value.trim();
+  const set = document.getElementById("set").value;
 
-      
-      if (!name) {
-        alert("名前を入力してください");
-        return;
-      }
-      sessionStorage.setItem("playerName", name);
-      sessionStorage.setItem("setName", set);
-      
-      window.location.href = "rule.html";
-    }
-  </script>
-  <script>
-  function adjustViewportHeight() {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  if (!name) {
+    alert("名前を入力してください");
+    return;
   }
+  sessionStorage.setItem("playerName", name);
+  sessionStorage.setItem("setName", set);
+      
+  window.location.href = "rule.html";
+}
 
-  window.addEventListener('resize', adjustViewportHeight);
-  window.addEventListener('load', adjustViewportHeight);
-</script>
+ function adjustViewportHeight() {
+   const vh = window.innerHeight * 0.01;
+   document.documentElement.style.setProperty('--vh', `${vh}px`);
+ }
+
+window.addEventListener('resize', adjustViewportHeight);
+window.addEventListener('load', adjustViewportHeight);
