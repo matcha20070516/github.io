1135ae0324fc116f539310fcf574411a67837da9
matcha20070516 <ma0516_hogreen@icloud.com>
2025-05-18 00:32:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function start() {
   const name = document.getElementById("name").value.trim();
-  const set = document.getElementById("set").value;
+  const select = document.getElementById("set");
+  const setText = select.options[select.selectesIndex].text;
 
   if (!name) {
     alert("名前を入力してください");
     return;
   }
   sessionStorage.setItem("playerName", name);
-  sessionStorage.setItem("setName", set);
+  sessionStorage.setItem("setName", setText);
       
   window.location.href = "rule.html";
 }
