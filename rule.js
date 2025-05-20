b3b7e476f87c52ee98207d662533b49573b150b9
matcha20070516 <ma0516_hogreen@icloud.com>
2025-05-20 11:28:38 +0900

    Update rule.js

--- rule.js
+++ rule.js
@@ -1,9 +1,4 @@
 function next() {
   const setName = localStorage.getItem("setName") || "";
-
-  if (setName.includes("謎検模試")) {
-    window.location.href = "excountdown.html";
-  } else {
-    window.location.href = "countdown.html";
-  }
+  window.location.href = "countdown.html";
 }
